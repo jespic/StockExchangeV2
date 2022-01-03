da2ea18928b09661ab0d3d2748b557ad4c6054a5
CORP\jespical <javiespicalabuig@gmail.com>
2022-01-03 21:10:30 +0100

    Implementation of DTOs done. Closes #1

---
+++ StockExchangeLib/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeLib.Models
+{
+    public class Country
+    {
+        public Country(string name)
+        {
+            Name = name;
+
+            Currencies = new HashSet<Currency>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string ApiName { get; set; }
+
+        //public CurrencyType CurrencyType { get; set; }
+
+        public ICollection<Currency> Currencies { get; set; }
+
+        public ICollection<Market> Markets { get; set; }
+    }
+}
---
+++ StockExchangeLib/Models/MyStock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchangeLib.Models
+{
+    public class MyStock
+    {
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public double BuyPrice { get; set; }
+
+        public DateTime BuyDate { get; set; }
+
+        public int StockId { get; set; }
+
+        public Stock Stock { get; set; }
+
+        public int BagUserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
