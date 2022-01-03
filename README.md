d42bad6cc15c1333522cf9de399c600cb728b05e
CORP\jespical <javiespicalabuig@gmail.com>
2022-01-03 17:41:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Stock Exchange
+Proyecto realizado en .NET Core para la gestión de acciones de bolsa bien hecho.
+
+
---
+++ StockExchange/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StockExchange.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
